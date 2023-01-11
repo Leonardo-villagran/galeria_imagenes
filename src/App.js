82e725904841ba27c3ce62f40348fbdaae90ce82
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-01-11 20:44:39 -0300

    json added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //Importación del archivo css segpun lo solicitado en desafío.
 import React from 'react'
 import './index.css';
-import './App.css';
+//Definición del arreglo para los contenidos de las distintas Cards.
+import data from './json/data.json';
+
 //Importación de boostrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 //Importación de Header, Card y Footer, según lo solicitado.
@@ -15,15 +17,7 @@ const contenido="Estos lenguajes de programación son los principales del 2023 y
 const email="mailto:leonardovillagran@yahoo.com";
 const color="primary";
 
-const nombre='@Leonardo-Villagrán. Todos los derechos reservados. ';
-
-//Definición del arreglo para los contenidos de las distintas Cards.
-const arreglo=[
-  {'id':1,'titulo':'C#','imagen':'https://images.pexels.com/photos/11035369/pexels-photo-11035369.jpeg', 'descripcion':'El relativamente joven lenguaje de programación C# (“C Sharp”, en su voz inglesa), lanzado en 2001, se considera un lenguaje de propósito general.'},
-  {'id':2,'titulo':'Pyhton', 'imagen':'https://images.pexels.com/photos/11035474/pexels-photo-11035474.jpeg', 'descripcion':'Python es un lenguaje de programación web de alto nivel basado en un código compacto, pero con una sintaxis fácil de entender.'},
-  {'id':3,'titulo':'Node JS', 'imagen':'https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg', 'descripcion':'Es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor,basado en el lenguaje de programación JavaScript.'},
-  {'id':4,'titulo':'PHP', 'imagen':'https://images.pexels.com/photos/11035390/pexels-photo-11035390.jpeg', 'descripcion':'El preprocesador de hipertexto, más conocido por sus siglas PHP, es un lenguaje de scripting escrito en Perl y C con licencia de código abierto.'}
-]
+const nombre='@Leonardo-Villagrán';
 
 //Impresión de la web principal
 function App() {
@@ -32,7 +26,7 @@ function App() {
       <Header title={title} color={color} />
       <div className="container">
         <div className="row justify-content-around">
-        {arreglo.map((elemento)=>(
+        {data['arreglo'].map((elemento)=>(
             <Card 
               key={elemento.id}
               titulo={elemento.titulo}
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,8 +1,9 @@
 //components/Footer.jsx
 import React from 'react'
 import Alert from 'react-bootstrap/Alert'
+import Badge from 'react-bootstrap/Badge';
 
-//Función que determina el formato del footer, se utiliza un Alert desde bootstrap según lo solicitado en la pregunta 4. 
+//Función que determina el formato del footer, se utiliza un Alert y un Badge desde bootstrap según lo solicitado en la pregunta 4. 
 const Footer = ({ contenido, email, nombre, color }) => {
     return (
         <div className='p-2'>
@@ -10,7 +11,8 @@ const Footer = ({ contenido, email, nombre, color }) => {
                 <div className="text-center">
                     <Alert.Heading>Importante:</Alert.Heading>
                     <p className="footer">{contenido}</p>
-                    <a href={email}>{nombre}</a>
+                    
+                    <a href={email}><Badge bg="secondary">{nombre}</Badge></a>
                 </div>
             </Alert>
         </div>
